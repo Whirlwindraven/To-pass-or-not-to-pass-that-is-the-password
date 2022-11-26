36e4f38385e70e19b05515faa97cfb49165fa774
Whirlwindraven <fadysherif1998@gmail.com>
2022-11-26 13:18:37 -0500

    added pseudo code

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,15 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword (){
   console.log('Hey! you clicked a button');
+
+// prompt user for password criteria 
+
+// Validate the input 
+
+// Generate passwrod based on criteria 
+
+// display password on the page 
+return "Generated password will go here!";
 }
 // Write password to the #password input
 function writePassword() {
