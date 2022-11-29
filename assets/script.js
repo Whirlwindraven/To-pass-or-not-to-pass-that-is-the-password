de047ec4cbe916f0c50372a7f8498bebf51e729f
Whirlwindraven <fadysherif1998@gmail.com>
2022-11-28 20:05:51 -0500

    wrote input validation code

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,11 @@
 // Variables for password use
 
 const passwordString = {
-  lowerCase: "abcedfghijklmnopqrstuvwxyz"
-  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  symbols: "!@#$%^&*()+=-§±~"
-  numbers: "0123456789"
-}
+  lowerCase: "abcedfghijklmnopqrstuvwxyz",
+  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  symbols: "!@#$%^&*()+=-§±~",
+  numbers: "0123456789",
+};
 
 
 // Assignment code here
@@ -17,11 +17,17 @@ let passwordSelection = "";
 // prompt user for password criteria 
 let length = window.prompt("What are the length requirments of your password ? Choose characters between 10 and 120 characters long.");
 // Validate the input 
+if (length < 10 || length > 120)
+window.confirm("Be advised to select a number between 10 and 120");
+return;
+} else
 
+console.log(length);
 // Generate passwrod based on criteria 
-
+let uppercase = window.confirm("Do you request uppercase characters?");
 // display password on the page 
 return "Generated password will go here!";
+}
 // Write password to the #password input
 function writePassword() {
   let password = generatePassword();
